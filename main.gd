eadcabd5807379e484e2c763d87e1970147d55d1
joris <jorisvddonk@gmail.com>
2020-03-17 20:25:09 +0100

    allow ship to shoot as well

--- main.gd
+++ main.gd
@@ -3,11 +3,16 @@ extends Node2D
 var Bullet = load("res://Bullet.tscn")
 
 func _ready():
-	$PlayerShip.connect("shoot", self, "_on_Player_shoot")
+	$PlayerShip.connect("shoot", self, "_on_Ship_shoot", [null, $PlayerShip])
+	$Ship.connect("shoot", self, "_on_Ship_shoot", [$Ship])
 
-func _on_Player_shoot():
+func _on_Ship_shoot(targetpos, ship):
+	print(ship, targetpos)
 	var bullet = Bullet.instance()
-	bullet.set_velocity($PlayerShip.velocity + Vector2(0, -300).rotated($PlayerShip.rotation))
-	bullet.position = $PlayerShip.position
-	bullet.rotation = $PlayerShip.rotation
+	var rot = ship.rotation
+	if targetpos != null:
+		rot = Vector2.UP.angle_to(targetpos - ship.position)
+	bullet.set_velocity(ship.velocity + Vector2(0, -300).rotated(rot))
+	bullet.position = ship.position
+	bullet.rotation = ship.rotation
 	add_child(bullet)
